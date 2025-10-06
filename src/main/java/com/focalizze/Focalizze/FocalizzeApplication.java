649c776abac27762f4035881ca3e34a919c42047
cjoriginal45 <joaquinribarola45@gmail.com>
2025-10-06 17:16:49 -0300

    first commit

---
+++ src/main/java/com/focalizze/Focalizze/FocalizzeApplication.java
@@ -0,0 +1,13 @@
+package com.focalizze.Focalizze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FocalizzeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FocalizzeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/focalizze/Focalizze/FocalizzeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.focalizze.Focalizze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FocalizzeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
