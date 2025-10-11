0443e38a223515bfcdbff5e8daa561f54de8367e
Matías Sillen Ríos <matias.sillen@gmail.com>
2025-10-11 00:54:56 -0300

    Refactoring some messages

--- src/main/java/com/focalizze/Focalizze/dto/RegisterRequest.java
+++ src/main/java/com/focalizze/Focalizze/dto/RegisterRequest.java
@@ -6,19 +6,19 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record RegisterRequest(
-        @NotBlank(message = "Username is required") //El nombre de usuario es obligatorio
-        @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters") //El nombre de usuario debe tener entre 3 y 50 caracteres.
+        @NotBlank(message = "El nombre de usuario es obligatorio") // Username is required
+        @Size(min = 3, max = 50, message = "El nombre de usuario debe tener entre 3 y 50 caracteres ") // Username must be between 3 and 50 characters
         String username,
 
-        @NotBlank(message = "Email is required") // El correo electrónico es obligatorio
-        @Email(message = "Email format is invalid") // El formato del correo electrónico no es válido
+        @NotBlank(message = "El correo electrónico es obligatorio") // Email is required
+        @Email(message = "El formato del correo electrónico no es válido") // Email format is invalid
         String email,
 
-        @NotBlank(message = "Password is required") // La contraseña es obligatoria
-        @Size(min = 6, message = "Password must be at least 6 characters") // La contraseña debe tener al menos 6 caracteres
+        @NotBlank(message = "La contraseña es obligatoria") // Password is required
+        @Size(min = 6, message = "La contraseña debe tener al menos 6 caracteres") // Password must be at least 6 characters
         String password,
 
-        @NotBlank(message = "Password confirmation is required") // La confirmación de la contraseña es obligatoria
+        @NotBlank(message = "La confirmación de la contraseña es obligatoria ") // Password confirmation is required
         String confirmPassword
 )
 { }
