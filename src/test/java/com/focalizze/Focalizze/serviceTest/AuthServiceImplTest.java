6d24775b541fd75da6ba09c30056c9817d72f074
Matías Sillen Ríos <matias.sillen@gmail.com>
2025-10-12 22:22:17 -0300

    I create the AuthServiceImplTest class

---
+++ src/test/java/com/focalizze/Focalizze/serviceTest/AuthServiceImplTest.java
@@ -0,0 +1,9 @@
+package com.focalizze.Focalizze.serviceTest;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class AuthServiceImplTest {
+    
+}
