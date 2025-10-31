798d54ee530dc494069ea436097c7c0c6b0658ec
cjoriginal45 <joaquinribarola45@gmail.com>
2025-10-31 14:18:53 -0300

    commentService and LikeService Update

--- src/main/java/com/focalizze/Focalizze/services/servicesImpl/LikeServiceImpl.java
+++ src/main/java/com/focalizze/Focalizze/services/servicesImpl/LikeServiceImpl.java
@@ -1,10 +1,13 @@
 package com.focalizze.Focalizze.services.servicesImpl;
 
+import com.focalizze.Focalizze.models.InteractionType;
 import com.focalizze.Focalizze.models.Like;
 import com.focalizze.Focalizze.models.ThreadClass;
 import com.focalizze.Focalizze.models.User;
+import com.focalizze.Focalizze.repository.InteractionLogRepository;
 import com.focalizze.Focalizze.repository.LikeRepository;
 import com.focalizze.Focalizze.repository.ThreadRepository;
+import com.focalizze.Focalizze.services.InteractionLimitService;
 import com.focalizze.Focalizze.services.LikeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -18,6 +21,8 @@ public class LikeServiceImpl implements LikeService {
 
     private final ThreadRepository threadRepository;
     private final LikeRepository likeRepository;
+    private final InteractionLimitService interactionLimitService;
+    private final InteractionLogRepository interactionLogRepository;
 
     @Override
     @Transactional
@@ -42,6 +47,10 @@ public class LikeServiceImpl implements LikeService {
             // Si el like no existe, lo creamos (dar like).
             // If the like does not exist, we create it (give like).
 
+            // Verificar si el usuario puede dar like.
+            interactionLimitService.checkInteractionLimit(currentUser);
+
+            // Si puede, creamos el like.
             Like newLike = Like.builder()
                     .user(currentUser)
                     .thread(thread)
@@ -50,6 +59,9 @@ public class LikeServiceImpl implements LikeService {
             // Actualizamos el contador.
             // We update the counter.
             thread.setLikeCount(thread.getLikeCount() + 1);
+
+            // Registramos la interacción.
+            interactionLimitService.recordInteraction(currentUser, InteractionType.LIKE);
         }
 
         // Guardamos la entidad del hilo con el contador actualizado.
