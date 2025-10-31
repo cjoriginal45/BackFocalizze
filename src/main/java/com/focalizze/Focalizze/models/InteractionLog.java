a7ad5a128293d103766a023c9f766ec8c241ce87
cjoriginal45 <joaquinribarola45@gmail.com>
2025-10-31 14:02:56 -0300

    interactions models

---
+++ src/main/java/com/focalizze/Focalizze/models/InteractionLog.java
@@ -0,0 +1,32 @@
+package com.focalizze.Focalizze.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "interaction_log_tbl")
+public class InteractionLog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private InteractionType type;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+}
