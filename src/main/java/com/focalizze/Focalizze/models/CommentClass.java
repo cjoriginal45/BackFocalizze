0dbdc87d6b8f4f4245600ae3cb89b3fcd07f061e
cjoriginal45 <joaquinribarola45@gmail.com>
2025-11-13 16:36:56 -0300

    comment fix

--- src/main/java/com/focalizze/Focalizze/models/CommentClass.java
+++ src/main/java/com/focalizze/Focalizze/models/CommentClass.java
@@ -1,6 +1,7 @@
 package com.focalizze.Focalizze.models;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -21,6 +22,7 @@ public class CommentClass {
     private Long id;
 
     @Column(nullable = false)
+    @Size(max = 281)
     private String content;
 
     private LocalDateTime createdAt;
