5d8164a67cd5722ae5cd831607d4a98fc50bb0c3
Matías Sillen Ríos <matias.sillen@gmail.com>
2025-11-16 17:19:20 -0300

    Modify the CategoryController class

--- src/main/java/com/focalizze/Focalizze/controllers/CategoryController.java
+++ src/main/java/com/focalizze/Focalizze/controllers/CategoryController.java
@@ -1,10 +1,15 @@
 package com.focalizze.Focalizze.controllers;
 
+import com.focalizze.Focalizze.dto.CategoryDetailsDto;
 import com.focalizze.Focalizze.dto.CategoryDto;
+import com.focalizze.Focalizze.dto.FeedThreadDto;
 import com.focalizze.Focalizze.models.User;
 import com.focalizze.Focalizze.services.CategoryFollowService;
 import com.focalizze.Focalizze.services.CategoryService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -32,5 +37,17 @@ public class CategoryController {
         User currentUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         categoryFollowService.toggleFollowCategory(id, currentUser);
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{name}")
+    public ResponseEntity<CategoryDetailsDto> getCategoryDetails(@PathVariable String name) {
+        return ResponseEntity.ok(categoryService.getCategoryDetails(name));
+    }
+
+    @GetMapping("/{name}/threads")
+    public ResponseEntity<Page<FeedThreadDto>> getThreadsByCategory(
+            @PathVariable String name,
+            @PageableDefault(size = 10) Pageable pageable) {
+        return ResponseEntity.ok(categoryService.getThreadsByCategory(name, pageable));
     }
 }
