34d654a5dffc8c2b044af682fbf256bfccd4c2eb
cjoriginal45 <joaquinribarola45@gmail.com>
2025-11-27 16:58:40 -0300

    block update implementation

--- src/main/java/com/focalizze/Focalizze/repository/BlockRepository.java
+++ src/main/java/com/focalizze/Focalizze/repository/BlockRepository.java
@@ -3,6 +3,8 @@ package com.focalizze.Focalizze.repository;
 import com.focalizze.Focalizze.models.Block;
 import com.focalizze.Focalizze.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 import java.util.Set;
@@ -14,7 +16,15 @@ public interface BlockRepository extends JpaRepository<Block, Long> {
     // Método para comprobar rápidamente si un bloqueo existe.
     boolean existsByBlockerAndBlocked(User blocker, User blocked);
 
-    Set<Long> findBlockedUserIdsByBlocker(Long id);
+    @Query("SELECT b.blocked.id FROM Block b WHERE b.blocker.id = :userId")
+    Set<Long> findBlockedUserIdsByBlocker(@Param("userId") Long userId);
 
-    Set<Long> findUserIdsWhoBlockedUser(Long id);
+    @Query("SELECT b.blocker.id FROM Block b WHERE b.blocked.id = :userId")
+    Set<Long> findUserIdsWhoBlockedUser(@Param("userId") Long userId);
+
+    @Query("SELECT b.blocked.id FROM Block b WHERE b.blocker = :blocker AND b.blocked.id IN :authorIds")
+    Set<Long> findBlockedIdsByBlockerAndBlockedIdsIn(
+            @Param("blocker") User blocker,
+            @Param("authorIds") Set<Long> authorIds
+    );
 }
