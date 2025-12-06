a6433805f9fd0260d4e01fccb510855d67765a68
Matías Sillen Ríos <matias.sillen@gmail.com>
2025-12-06 17:07:23 -0300

    Add a new endpoint for validatePassword in the SecurityController class

--- src/main/java/com/focalizze/Focalizze/controllers/SecurityController.java
+++ src/main/java/com/focalizze/Focalizze/controllers/SecurityController.java
@@ -4,9 +4,12 @@ import com.focalizze.Focalizze.dto.TwoFactorRequestDto;
 import com.focalizze.Focalizze.models.User;
 import com.focalizze.Focalizze.services.SecurityService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/security")
@@ -32,5 +35,21 @@ public class SecurityController {
     ) {
         securityService.logoutAllDevices(currentUser);
         return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/validate-password")
+    public ResponseEntity<Boolean> validatePassword(
+            @RequestBody Map<String, String> request,
+            @AuthenticationPrincipal User currentUser
+    ) {
+        String password = request.get("password");
+        boolean isValid = securityService.validatePassword(password, currentUser);
+
+        if (isValid) {
+            return ResponseEntity.ok(true);
+        } else {
+            // Devolvemos 400 o 403 si está mal
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(false);
+        }
     }
 }
