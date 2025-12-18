01527137ccb8b598817623110c184be9e68af017
cjoriginal45 <joaquinribarola45@gmail.com>
2025-12-18 12:43:00 -0300

    exceptions refactor

--- src/main/java/com/focalizze/Focalizze/exceptions/DailyLimitExceededException.java
+++ src/main/java/com/focalizze/Focalizze/exceptions/DailyLimitExceededException.java
@@ -3,7 +3,11 @@ package com.focalizze.Focalizze.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.TOO_MANY_REQUESTS) // devuelve un 429
+/**
+ * Exception thrown when a user exceeds their daily operational limits.
+ * Returns 429 Too Many Requests.
+ */
+@ResponseStatus(HttpStatus.TOO_MANY_REQUESTS)
 public class DailyLimitExceededException extends RuntimeException {
     public DailyLimitExceededException(String message) {
         super(message);
